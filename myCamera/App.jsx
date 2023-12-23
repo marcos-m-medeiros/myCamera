ebe243c13d3330f909b222bc5515712e9100e93b
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-12-23 16:54:47 -0300

    Botão de trocar câmera e botão de tirar foto
    - Importação da TouchableOpacity
    
    - Ref do botão flip
    Um botão foi adicionado para alternar entre as câmeras frontal e traseira.
    
    - Ref do botão de captura da câmera
    Outro botão foi adicionado para capturar uma foto.
    
    -Estilos adicionados
    Estilos foram adicionados para os novos botões e o container dos botões

--- myCamera/App.jsx
+++ myCamera/App.jsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { Camera } from "expo-camera";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
+  const camRef = useRef(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [capturedPhoto, setCaputuredPhoto] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -21,9 +30,34 @@ export default function App() {
     return <Text>Acesso Negado</Text>;
   }
 
+  async function takePicture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCaputuredPhoto(data.uri);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Camera style={styles.camera} type={type}></Camera>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <FontAwesome name="exchange" size={23} color="red" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonCamera} onPress={takePicture}>
+            <FontAwesome name="camera" size={23} color={"#fff"} />
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </SafeAreaView>
   );
 }
@@ -36,5 +70,34 @@ const styles = StyleSheet.create({
   camera: {
     width: "100%",
     height: "100%",
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  buttonCamera: {
+    position: "absolute",
+    bottom: 50,
+    right: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
   },
 });
